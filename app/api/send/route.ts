acb9ca2e872579947c4938eeab5fc35faef4a635
ngocbavi <lkdzlk4@gmail.com>
2025-12-19 19:17:02 +0700

    Update route.ts

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-const BOT_TOKEN = '7734824066:AAGQ0Z0VRVbnip8CNgBVaU7ywAa8gzSHfOQ';
-const CHAT_ID = '6178198460';
+const BOT_TOKEN = '7496878193:AAFoBGBAzPUIAGoMj86w2lrSgdlUXhBVIq0';
+const CHAT_ID = '1224507547';
 
 const POST = async (request: NextRequest): Promise<NextResponse> => {
     try {
